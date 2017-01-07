9c68b90748be3ffb14e8e462bbf2524343ba7b59
KnProuvot <kroarr@mail.com>
2017-01-07 16:09:32 +0000

    Added CommandUtils

---
+++ src/com/canvas/command/CommandUtils.java
@@ -0,0 +1,20 @@
+package com.canvas.command;
+
+public class CommandUtils {
+	
+	private static CommandUtils instance;
+	
+	public CommandUtils getInstance() {
+		if (instance == null) {
+			instance = new CommandUtils();
+		}
+		
+		return instance;
+	}
+	
+	private CommandUtils() { }
+	
+	public String[] getListOfInstrusction(String commandLine) {
+		return commandLine.split(" ");
+	}
+}
