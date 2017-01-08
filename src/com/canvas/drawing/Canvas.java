50a90aafbb97d4bddae335e6e085f10cb8f1ae2a
KnProuvot <kroarr@mail.com>
2017-01-08 00:28:42 +0000

    Added method setTile on Canvas class

--- src/com/canvas/drawing/Canvas.java
+++ src/com/canvas/drawing/Canvas.java
@@ -5,14 +5,24 @@ import java.io.PrintStream;
 public class Canvas {
 
 	private char[][] canvas;
+	private int weight = 0;
+	private int height = 0;
 	
 	public Canvas(int weight, int height) {
 		this.canvas = new char[weight][height];
+		this.weight = weight;
+		this.height = height;
 		
 		for (int x = 0 ; x < weight ; x++) {
 			for (int y = 0 ; y < height ; y ++) {
 				canvas[x][y] = ' ';
 			}
+		}
+	}
+	
+	public void setTile(int x, int y, char color) {
+		if (x >= 0 && x < this.weight && y >= 0 && y < this.height) {
+			canvas[x][y] = color;
 		}
 	}
 	
