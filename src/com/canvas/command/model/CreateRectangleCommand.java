4170b19a1d9da4caad3bd7c3158fbb96489cdd65
KnProuvot <kroarr@mail.com>
2017-01-08 14:17:53 +0000

    Added Rectanble drawing implementation

--- src/com/canvas/command/model/CreateRectangleCommand.java
+++ src/com/canvas/command/model/CreateRectangleCommand.java
@@ -17,6 +17,19 @@ public class CreateRectangleCommand implements Command {
 		this.y1 = Integer.valueOf(instruction[2]);
 		this.x2 = Integer.valueOf(instruction[3]);
 		this.y2 = Integer.valueOf(instruction[4]);
+		
+		// Making sure that x1 is always the top left coordinate and y2 the bottom right
+		if (x1 > x2) {
+			int buffer = x1;
+			x1 = x2;
+			x2 = buffer;
+		}
+		
+		if (y1 > y2) {
+			int buffer = y1;
+			y1 = y2;
+			y2 = buffer;
+		}
 	}
 
 	public int getX1() {
@@ -37,8 +50,22 @@ public class CreateRectangleCommand implements Command {
 
 	@Override
 	public void apply(Canvas canvas) {
-		// TODO Auto-generated method stub
-		
+		// Top side
+		for (int x = x1 ; x <= x2 ; x++) {
+			canvas.setTile(x, y1, DEFAULT_COLOR);
+		}
+		// Bottom side
+		for (int x = x1 ; x <= x2 ; x++) {
+			canvas.setTile(x, y2, DEFAULT_COLOR);
+		}
+		// Left side
+		for (int y = y1 ; y <= y2 ; y++) {
+			canvas.setTile(x1, y, DEFAULT_COLOR);
+		}
+		// Right side
+		for (int y = y1 ; y <= y2 ; y++) {
+			canvas.setTile(x2, y, DEFAULT_COLOR);
+		}
 	}
 
 }
